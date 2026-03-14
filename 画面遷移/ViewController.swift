ff332c15c52607b00527467199eb86b50c7a2b93
atomseito <eitoeito.atoms@gmail.com>
2026-03-14 13:45:32 +0900

    add screen transition prototype

---
+++ 画面遷移/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  画面遷移
+//
+//  Created by 上條裕子 on 2026/01/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
